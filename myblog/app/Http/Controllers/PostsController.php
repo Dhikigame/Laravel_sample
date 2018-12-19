9b4c195a1332c051535c8981209ebdb67fbc05de
daiki <dadada7772000@yahoo.co.jp>
2018-12-19 23:36:49 +0900

    Implicit Binding追加

--- myblog/app/Http/Controllers/PostsController.php
+++ myblog/app/Http/Controllers/PostsController.php
@@ -14,4 +14,9 @@ class PostsController extends Controller
         //dd($posts->toArray());
         return view('posts.index')->with('posts', $posts);
     }
+    public function show(Post $post) {
+        // $post = Post::find($id);
+        // $post = Post::findOrFail($id);
+        return view('posts.show')->with('post', $post);
+      }
 }
