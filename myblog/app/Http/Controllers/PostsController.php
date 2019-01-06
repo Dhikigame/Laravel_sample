b17f2584c6f950fb559d9432f5e586c667aff13f
daiki <dadada7772000@yahoo.co.jp>
2019-01-06 23:54:02 +0900

    記事のdelete処理の実装

--- myblog/app/Http/Controllers/PostsController.php
+++ myblog/app/Http/Controllers/PostsController.php
@@ -45,5 +45,9 @@ class PostsController extends Controller
         $post->save();
         return redirect('/');
       }
-  
+
+    public function destroy(Post $post) {
+        $post->delete();
+        return redirect('/');
+    }
 }
