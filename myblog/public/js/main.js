062c8a434220e1c6317c10754a730ac032a0af6d
daiki <dadada7772000@yahoo.co.jp>
2019-01-08 23:36:18 +0900

    Commentモデルの作成(記事にコメントつけられるようにする)

--- myblog/public/js/main.js
+++ myblog/public/js/main.js
@@ -2,10 +2,11 @@
     'use strict';
     var cmds = document.getElementsByClassName('del');
     var i;
-  
+    // console.log(cmds.length);
     for (i = 0; i < cmds.length; i++) {
-      cmds[i].addEventListener('click', function(e) {
-        e.preventDefault();
+        cmds[i].addEventListener('click', function(e) {
+        // console.log(i);
+        // e.preventDefault();
         if (confirm('are you sure?')) {
           document.getElementById('form_' + this.dataset.id).submit();
         }
