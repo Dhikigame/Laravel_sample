0a5ee12f5bee28580468f000dac80827ec3eded1
daiki <dadada7772000@yahoo.co.jp>
2019-02-26 23:46:10 +0900

    コメント消去

--- myblog/public/js/main.js
+++ myblog/public/js/main.js
@@ -2,11 +2,8 @@
     'use strict';
     var cmds = document.getElementsByClassName('del');
     var i;
-    // console.log(cmds.length);
     for (i = 0; i < cmds.length; i++) {
         cmds[i].addEventListener('click', function(e) {
-        // console.log(i);
-        // e.preventDefault();
         if (confirm('are you sure?')) {
           document.getElementById('form_' + this.dataset.id).submit();
         }
